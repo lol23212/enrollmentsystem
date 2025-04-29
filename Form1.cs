093775649e5a46bbd34f49c0035f6b9fb6c09615
lol23212 <nhielradores9@gmail.com>
2025-04-29 13:57:38 +0800

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -16,7 +16,8 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
-
+        MySqlConnection con = new MySqlConnection(@"Data Source=localhost;Initial Catalog=personal_info;uid=root;pwd=qaz123456789;database=students");
+        MySqlCommand cmd = new MySqlCommand();
         private void Form1_Load(object sender, EventArgs e)
         {
 
@@ -46,5 +47,69 @@ namespace WindowsFormsApp1
         {
 
         }
+        private void button1_Click(object sender, EventArgs e)
+{
+    if (txtlogin.Text == "" ^ txtpassword.Text == "")
+    {
+        label2.Text = "Please enter your username";
+        label7.Text = "Please enter your password";
+    }
+
+    string login = txtlogin.Text;
+    if (int.TryParse(login, out int num))
+    {
+
+        string checkuser = "SELECT exists (select 1 FROM enrolled_students WHERE student_num ='" + txtlogin.Text + "');";
+        string checkpass = "SELECT exists (select 1 FROM enrolled_students WHERE pass ='" + txtpassword.Text + "');";
+
+
+
+        int use;
+        int pass;
+        con.Open();
+        try
+        {
+            MySqlCommand cmd = new MySqlCommand(checkuser, con);
+            use = Convert.ToInt32(cmd.ExecuteScalar());
+
+            cmd = new MySqlCommand(checkpass, con);
+            pass = Convert.ToInt32(cmd.ExecuteScalar());
+
+
+
+
+
+            if (use == 0)
+            {
+                label7.Text = "wrong password";
+            }
+            if (pass == 0)
+            {
+                label1.Text = "wrong username";
+            }
+            else if (use == 1 && pass == 1)
+            {
+                string studentnum = txtlogin.Text;
+                string studentpass = txtpassword.Text;
+                formstudent student = new formstudent(studentnum, studentpass);
+                student.Show();
+                this.Hide();
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+        }
+        finally
+        {
+            con.Close();
+        }
+    }
+    else
+    {
+        label1.Text = "wrong username";
+        label7.Text = "wrong password";
+    }
+}
     }
 }
